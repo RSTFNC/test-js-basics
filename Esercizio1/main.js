f465844ba55991cacb4c27b24c8d4338326e99b6
Franco Rosato <sigma0934@gmail.com>
2023-11-10 11:28:47 +0100

    feat | esercizio 1

--- Esercizio1/main.js
+++ Esercizio1/main.js
@@ -1,22 +1,20 @@
-function analizzaTesto(testo) {
-	// Esegui l'analisi del testo e crea un oggetto con le informazioni desiderate
-	const lunghezzaTesto = testo.length;
-	const parole = testo.split(" ").length;
-	const caratteriSpeciali = testo.match(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/g);
-	const numCaratteriSpeciali = caratteriSpeciali ? caratteriSpeciali.length : 0;
-
-	// Crea l'oggetto con le informazioni raccolte
+function analizzaTesto(text) {
+	const originale = text;
+	const minuscolo = text.toLowerCase();
+	const senzaSpazi = text.trim();
+	const numeroCaratteri = text.length;
+	const invertita = text.split("").reverse().join("");
 	const risultato = {
-		lunghezzaTesto,
-		parole,
-		numCaratteriSpeciali,
+		originale,
+		minuscolo,
+		senzaSpazi,
+		numeroCaratteri,
+		invertita,
 	};
-
 	return risultato;
 }
 
-// Esempio di utilizzo della funzione
-const testoDiEsempio = "Questo è un esempio di testo! Contiene delle parole.";
-const risultatoAnalisi = analizzaTesto(testoDiEsempio);
+const testo = " Javasript é fantastico ";
+const risultatoAnalisi = analizzaTesto(testo);
 
 console.log(risultatoAnalisi);
